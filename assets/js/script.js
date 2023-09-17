327c4e89a9f3cb3dcb3dc732aa90a674a3a47f23
Sakura396 <sakusaku00707@gmal.com>
2023-09-17 22:03:46 +0900

    JSいいね欄挑戦中

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,16 +42,20 @@ function textcopy() {
     var textarea = document.getElementById("textarea").value;
     document.getElementById("textarea").value = "";
     var counter = 0;
+    var moji ="testID"
+    var tmp =document.getElementByClassName("frame");
+    for(var i=0;i<tpm.length-1;i++){
+        tmp[i].setAttribute("id",moji+i);
+    }
     textarea = "<div class='frame'>" + "<p class='problem'>" + textarea + "</p>" +
-      "<button type='button' + class='far fa-thumbs-up' id='goodbutton'>" +
+      "<button type='button' + class='far fa-thumbs-up' id='testID'>" +
       "できた👍" + "</button>" + "<a id='clicks'>" + counter + "</a>" +
       "</div>" + document.getElementById("insert").innerHTML;
     document.getElementById("insert").innerHTML = textarea;
-    document.getElementById("goodbutton").onclick = function() {
+
+    document.getElementById("testID").onclick = function() {
       counter += 1;
       document.getElementById("clicks").innerHTML = counter;
     }
   }
 
-
->>>>>>> origin/feature/sakura2
